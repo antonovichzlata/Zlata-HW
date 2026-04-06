57d58aa70e5f048b1b0639058a4eba3dfe3e5cfb
Zlata <antonovich.zlata@gmail.com>
2026-04-06 21:41:53 +0300

    HW48

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //тільки повторювані [1, 3]
 
 const a = [1, 1, 2, 3, 3, 7];
-const repeat = a.filter((el) => a.indexOf(el) !== a.lastIndexOf(el));
+const repeat = a.filter(el => a.indexOf(el) !== a.lastIndexOf(el));
 const set = new Set(repeat);
 const repeatResult = Array.from(set);
 console.log(repeatResult);
@@ -16,7 +16,8 @@ function isUnique (arr) {
 //const arr = [1, 2, 2, 3, 4, 4, 5]; тільки елементи, що повторюються 1 раз => [1, 3, 5]
 
 const arr = [1, 2, 2, 3, 4, 4, 5];
-const unique = a.filter((el) => a.indexOf(el) === a.lastIndexOf(el));
+const unique = a.filter(el => a.indexOf(el) === a.lastIndexOf(el));
+console.log(unique);
 
 //4. const set = new Set();
 //set.add([1,2]);
