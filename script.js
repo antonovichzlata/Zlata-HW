86d1869eeb19b08e19472ec76899fc0fae8c6707
Zlata <antonovich.zlata@gmail.com>
2026-07-15 14:57:02 +0300

    HW 68

---
+++ script.js
@@ -0,0 +1,28 @@
+fetch("https://picsum.photos/v2/list")
+  .then(res => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw new Error("Bad status");
+  })
+  .then(images => {
+    const gallery = document.querySelector(".gallery");
+    images.forEach(image => {
+        gallery.insertAdjacentHTML(
+            "beforeend",
+            `
+            <div class="card">
+                <img src="${image.download_url}" alt="${image.author}">
+                <p>${image.author}</p>
+                <a href="${image.download_url}" target="_blank">
+                    Завантажити
+                </a>
+            </div>
+            `
+        )
+    });
+
+  })
+  .catch(err => {
+    console.error(err)
+  });
